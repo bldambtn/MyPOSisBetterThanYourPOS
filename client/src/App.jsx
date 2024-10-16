7cb76fc0c51a64b5df26208c35a2194ce0d8ff12
Sherman Burwell <shermanburwell3@gmail.com>
2024-10-15 20:41:31 -0500

    Removed unnecessary code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 
 const httpLink = createHttpLink({
