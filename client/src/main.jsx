c3c402734ed0c2509859a849d1c3e5a14916fefe
Sherman Burwell <shermanburwell3@gmail.com>
2024-10-15 20:11:04 -0500

    Added bloilerplate code to main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        error: <NoMatch />,
+        children: [
+            {
+            index: true, 
+            element: <Home />
+            },
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
