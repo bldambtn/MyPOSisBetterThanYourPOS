414025fda6fc5414c2de6c16c2341361cd568b2b
unknown <travihay@gmail.com>
2024-10-16 19:06:03 -0500

    added loginModal

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import LoginModal from './LoginModal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
     <div className="container">
+      <h1>Welcome to the Home Page</h1>
       <p>Hello World!</p>
+      <LoginModal />
     </div>
   );
 };
 
 export default Home;
+
