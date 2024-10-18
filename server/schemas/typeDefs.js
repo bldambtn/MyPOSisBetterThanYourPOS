a3cef6d2cc9007d02fa5f2d70372953eea76b2f7
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-17 19:19:18 -0500

    added JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,44 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
     hello: String
     getItems: [Item]
+    user: User # 'user' query
   }
 
   type Item {
     id: ID!
     name: String!
     quantity: Int!
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Mutation {
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+    ): User
+    login(email: String!, password: String!): Auth
   }
 `;
 
