c5bae86d9897ea8d0ac775581c038fe21fca8592
unknown <travihay@gmail.com>
2024-10-19 14:25:30 -0500

    fixed duplicate code

---
+++ client/src/components/TitleBanner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TitleBanner = () => {
+  return (
+    <h1 className="title-banner">Superior Supply.io</h1>
+  );
+};
+
+export default TitleBanner;
---
+++ client/src/pages/PointOfSale.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ActionButtons from '../components/ActionButtons';
+import EntryField from '../components/EntryField';
+import ItemScreen from '../components/ItemScreen';
+import Totals from '../components/Totals';
+import WarningMessage from '../components/WarningMessage';
+
+const PointOfSale = () {
+    return (
+        <div className="pos">
+            <div className="warning-text">
+                <WarningMessage />
+            </div>
+            <div className="item-screen">
+                <ItemScreen />
+            </div>
+            <div className="totals">
+                <Totals />
+            </div>
+            <div className="action-buttons">
+                <ActionButtons />
+            </div>
+            <div className="entry-field">
+                <EntryField />
+            </div>
+        </div>
+    );
+};
