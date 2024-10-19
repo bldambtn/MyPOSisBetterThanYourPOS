1d71f13e3fc8e8bd84658bd05e980264d7cc9f69
Sherman Burwell <shermanburwell3@gmail.com>
2024-10-19 13:20:50 -0500

    Added psuedocode to PointOfSale.jsx

---
+++ client/src/pages/PointOfSale.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PointOfSale = () {
+    return (
+        // <WarningMessage />
+        // <ItemScreen />
+        // <Totals />
+        // <Actions />
+        // <EntryField />
+    );
+};
