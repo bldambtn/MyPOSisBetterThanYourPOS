d3bf10ac4c8e90faacbc3a88f5f7ab370b57f70b
nhl1090 <neilhdz24@gmail.com>
2024-10-23 01:10:27 +0100

    Changed inventory back to one table.

--- client/src/pages/InventoryDashboard.jsx
+++ client/src/pages/InventoryDashboard.jsx
@@ -21,33 +21,6 @@ const InventoryDashboard = () => {
   return (
     <div>
       <h1>Inventory Dashboard</h1>
-      <h2>Inventory Schema</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Field Name</th>
-            <th>Data Type</th>  {/* Field data type (string, number, etc.) */}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>UPC</td>
-            <td>String</td>  {}
-          </tr>
-          <tr>
-            <td>Product Name</td>
-            <td>String</td>  {}
-          </tr>
-          <tr>
-            <td>In Stock</td>
-            <td>Number</td>  {}
-          </tr>
-          <tr>
-            <td>Sale Price</td>
-            <td>Number</td>  {}
-          </tr>
-        </tbody>
-      </table>
 
       <table>
         <thead>
