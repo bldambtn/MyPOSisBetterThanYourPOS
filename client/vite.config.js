e6b5e8efb1c5e2884193f92bb20fd8d6f51c234d
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-25 22:21:39 -0500

    reverted package.json and vite.config file to design2 state, prior to test

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3001, // Dynamically set port
+    port: 3000,
     open: true,
     proxy: {
       "/graphql": {
-        target: `http://localhost:${process.env.PORT || 3000}`, // Ensures backend connection
+        target: "http://localhost:3001",
         secure: false,
         changeOrigin: true,
       },
