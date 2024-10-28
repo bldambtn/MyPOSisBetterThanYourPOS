78accceeab4018580e7dbd089b6453fd8ee35af2
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-10-27 23:41:08 -0500

    updated no match styling

--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -1,14 +1,28 @@
+import React from 'react';
+import Header from '../components/TitleBanner';
+import Footer from '../components/Footer';
 import '../index.css';
 
 const NoMatch = () => {
   return (
-    <div>
-        <h1>404 Page Not Found</h1>
+    <div className="nomatch-page">
+      <Header />
+      <div className="content">
+        <h1>The page you were looking for didn't show up to work today.</h1>
+        <p>Let's go somewhere it's on duty</p>
         <h1>
           <span role="img" aria-label="Face With Rolling Eyes Emoji">
             🙄
           </span>
         </h1>
+        <button
+          className="home-button"
+          onClick={() => (window.location.href = "/")}
+        >
+          Go to Home
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 };
